29574dd73ac37903102de54cc95f29a31c4d1623
Pratik Mothe <pratikmothe02@gmail.com>
2022-11-21 19:19:53 +0530

    feat: Npm dependencies
    feat: React, React-dom and parcel.js

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { Button } from './Components/Button';
+export function App() {
+  return (
+    <div>
+      <Button></Button>
+      <h1> k1 Hello world from React! Parcel js</h1>
+    </div>
+  );
+}
