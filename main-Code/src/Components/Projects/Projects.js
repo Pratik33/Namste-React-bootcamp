8475b4e1b1ac4ac3687855c50ca46528ee26c192
Pratik Mothe <pratikmothe02@gmail.com>
2022-11-30 10:40:36 +0530

    refactor : header - footer and projectsection changed
    refactor : header - footer and projectsection changed

--- main-Code/src/Components/Projects/Projects.js
+++ main-Code/src/Components/Projects/Projects.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ProjectItem from './Project/ProjectItem';
 
 function Projects({ assignments }) {
-  let projects = assignments.map((assignment) => {
-    return <ProjectItem assignment={assignment} />;
+  let projects = assignments.map((assignment, index) => {
+    let cssClass = index % 2 == 0 ? 'offset-background' : 'plain-background:';
+    return (
+      <ProjectItem
+        assignment={assignment}
+        cssClass={cssClass}
+      />
+    );
   });
   return <>{projects}</>;
 }
